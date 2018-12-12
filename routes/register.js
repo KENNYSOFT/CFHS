a711eb6d28282c805bc69a1175fab769507cc5f0
KENNYSOFT <hyeonmin.park@kennysoft.kr>
2018-12-12 21:50:57 +0900

    r_list: Sort, search

--- routes/register.js
+++ routes/register.js
@@ -12,9 +12,12 @@ router.post('/first_step', wrapper.asyncMiddleware(async (req, res, next) => {
     var sess = req.session;
     var rows = await db.getQueryResult('SELECT * FROM user WHERE Uid = ?;', [req.body.U_id]);
     if(rows.length !== 0) res.render('fail_reg', {session : req.session});
-    else{
+    else {
 
         if(req.body.user_t === 'free'){
+            rows = await db.getQueryResult('SELECT * FROM team WHERE Tname = ?;', [req.body.U_id]);
+            if(rows.length !== 0) res.render('fail_reg', {session : req.session});
+
             sess.tempId = req.body.U_id;
             sess.tempName = req.body.name;
             sess.tempPnumber = req.body.Pnumber;
@@ -22,7 +25,8 @@ router.post('/first_step', wrapper.asyncMiddleware(async (req, res, next) => {
             sess.tempUserType = req.body.user_t;
 
 
-          await db.getQueryResult('INSERT INTO user(Password, Name, Uid, Usertype) VALUES (PASSWORD(?), ?, ?, ?);', [req.body.pwd, req.body.name, req.body.U_id, 1]);
+            await db.getQueryResult('INSERT INTO user(Password, Name, Uid, Usertype) VALUES (PASSWORD(?), ?, ?, ?);', [req.body.pwd, req.body.name, req.body.U_id, 1]);
+            await db.getQueryResult('INSERT INTO team(Tname, Leader_id, Solo) VALUES (?, ?, ?);', [req.body.U_id, req.body.U_id, 1]);
             res.render('free', {session: req.session});
         }
 
@@ -38,10 +42,10 @@ router.post('/first_step', wrapper.asyncMiddleware(async (req, res, next) => {
 
 router.post('/second_step_free', wrapper.asyncMiddleware(async (req, res, next) => {
   var sess = req.session;
-  await db.getQueryResult('INSERT INTO user_free(Pnumber, Career_year, Major, Age, UFid) VALUES (?, ?, ?, ?, ?)', [sess.tempPnumber, req.body.career_year, req.body.major, req.body.age, sess.tempId]);
+  await db.getQueryResult('INSERT INTO user_free(Pnumber, Career_year, Major, Age, UFid) VALUES (?, ?, ?, ?, ?);', [sess.tempPnumber, req.body.career_year, req.body.major, req.body.age, sess.tempId]);
   for (var i = 1; i <= req.body.langcnt; ++i)
   {
-      await db.getQueryResult('INSERT INTO uses(Ulevel, UFid, Lname) VALUES (?, ?, ?)', [req.body["language_level_" + i], sess.tempId, req.body["language_name_" + i]]);
+      await db.getQueryResult('INSERT INTO uses(Ulevel, UFid, Lname) VALUES (?, ?, ?);', [req.body["language_level_" + i], sess.tempId, req.body["language_name_" + i]]);
   }
   res.render('fin_reg', {session : req.session});
   res.end();
--- routes/request.js
+++ routes/request.js
@@ -4,32 +4,47 @@ var wrapper = require('../modules/wrapper');
 var db = require('../modules/db');
 
 router.get('/list', wrapper.asyncMiddleware(async (req, res, next) =>  {
-  var rows = await db.getQueryResult("SELECT * from request", []);
+  var rows = await db.getQueryResult("SELECT * FROM request JOIN user ON request.URid = user.Uid", []);
   res.render('r_list', {
       session: req.session,
       rows : rows});
 }));
 
+router.get('/search/:type', wrapper.asyncMiddleware(async (req, res, next) =>  {
+  var rows;
+  switch (req.params.type)
+  {
+    case "finding":
+      rows = await db.getQueryResult("SELECT * FROM request JOIN user ON request.URid = user.Uid WHERE Tname = ?;", ["admin"]);
+      break;
+    case "applicable":
+      rows = await db.getQueryResult("SELECT * FROM (SELECT * FROM request JOIN user ON request.URid = user.Uid) A WHERE Tname = ? AND Required_min_count = ? AND Required_max_count = ? AND NOT EXISTS (SELECT * FROM requires B WHERE B.Rid = A.Rid AND NOT EXISTS (SELECT * FROM uses C WHERE B.Lname = C.Lname AND C.UFid = ? AND C.Ulevel >= B.Rlevel));", ["admin", 1, 1, req.session.uid]);
+      break;
+    case "proceeding":
+      rows = await db.getQueryResult("SELECT * FROM request JOIN user ON request.URid = user.Uid WHERE Tname = ?;", [req.session.uid]);
+      break;
+    case "requested":
+      rows = await db.getQueryResult("SELECT * FROM request JOIN user ON request.URid = user.Uid WHERE Rid IN (SELECT Rid FROM applies WHERE Tname = ?);", [req.session.uid]);
+      break;
+  }
+  res.render('r_list', {
+    session: req.session,
+    rows : rows});
+}));
+
+router.get('/sort/:legend/:order', wrapper.asyncMiddleware(async (req, res, next) =>  {
+  var rows = await db.getQueryResult("SELECT * FROM request JOIN user ON request.URid = user.Uid ORDER BY " + req.params.legend + " " + req.params.order + ";");
+  res.render('r_list', {
+    session: req.session,
+    rows : rows,
+    legend: req.params.legend, order: req.params.order});
+}));
+
 router.get('/make', wrapper.asyncMiddleware(async (req, res, next) =>  {
-    var rows = await db.getQueryResult("SELECT * from request", []);
-    res.render('r_make', {
-        session: req.session
-    });
-  }));
-
-router.post('/search', wrapper.asyncMiddleware(async (req, res, next) =>  {
-    var rows = await db.getQueryResult("SELECT * from request", []);
-    res.render('r_list', {
-        session: req.session,
-        rows : rows});
-  }));
-
-router.post('/sort', wrapper.asyncMiddleware(async (req, res, next) =>  {
-    var rows = await db.getQueryResult("SELECT * from request", []);
-    res.render('r_list', {
-        session: req.session,
-        rows : rows});
-  }));
-
+  var rows = await db.getQueryResult("SELECT * FROM request JOIN user ON request.URid = user.Uid;", []);
+  res.render('r_make', {
+    session: req.session
+  });
+}));
 
 module.exports = router;
